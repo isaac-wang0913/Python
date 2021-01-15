a434fb4e689a54bed986a2abf405443c8194956b
isaac-wang0913 <74162153+isaac-wang0913@users.noreply.github.com>
2021-01-15 11:27:22 +0800

    Update 单链表.py

--- algorithm_and_data_structure/链表/单链表.py
+++ algorithm_and_data_structure/链表/单链表.py
@@ -15,6 +15,8 @@ class SingleLinkList:
     def __init__(self):
         self.size = 0
         # 哨兵节点被用作伪头始终存在，这样结构中永远不为空，它将至少包含一个伪头
+        # 哨兵节点广泛应用于树和链表中，如伪头、伪尾、标记等，它们是纯功能的，通常不保存任何数据，
+        # 其主要目的是使链表标准化，如使链表永不为空、永不无头、简化插入和删除
         self.head = ListNode(0) # 哨兵节点
 
     def get(self, index):
