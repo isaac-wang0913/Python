8f9b989d8dfa97c1d7905f9c7a8f7c37660bb4bf
isaac-wang0913 <74162153+isaac-wang0913@users.noreply.github.com>
2021-01-15 15:55:13 +0800

    Update 移除链表元素.py

--- algorithm_and_data_structure/链表/移除链表元素.py
+++ algorithm_and_data_structure/链表/移除链表元素.py
@@ -1,25 +1,24 @@
-# https://leetcode-cn.com/leetbook/read/linked-list/f9izv/
-
+# https://leetcode-cn.com/leetbook/read/linked-list/fe0kj/
+# Definition for singly-linked list.
 class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
-    def removeElements(self, head, val):
+    def oddEvenList(self, head):
         # 双指针
         # 时间复杂度：O(n)
         # 空间复杂度：O(1)
-        # 哨兵节点广泛应用于树和链表中，如伪头、伪尾、标记等，它们是纯功能的，通常不保存任何数据，
-        # 其主要目的是使链表标准化，如使链表永不为空、永不无头、简化插入和删除
-        dummy = ListNode(0)
-        dummy.next = head
-        pre = dummy
-        cur = head
-        while cur:
-            if cur.val == val:
-                pre.next = cur.next
-            else:
-                pre = cur   # 避免出现连续的val值
-            cur = cur.next  # 无论是cur.val == val 还是 cur.val != val ，cur都要更新迭代
-        return dummy.next
+        if not head:
+            return
+        evenhead = head.next    # 偶数头
+        odd = head              # 奇数开头
+        even = head.next
+        while even and even.next:   # while odd 会溢出
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = evenhead
+        return head
