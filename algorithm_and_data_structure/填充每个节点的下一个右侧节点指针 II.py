676acc6e0b8dbc557c23cfe2c20db70a9040beeb
isaac-wang0913 <74162153+isaac-wang0913@users.noreply.github.com>
2021-01-25 15:35:54 +0800

    Create 填充每个节点的下一个右侧节点指针 II.py

---
+++ algorithm_and_data_structure/填充每个节点的下一个右侧节点指针 II.py
@@ -0,0 +1,27 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def connect(self, root:):
+        # 广度优先需要用队列作为辅助结构
+        # 时间复杂度：O(n)
+        # 空间复杂度：O(n)
+        if not root:
+            return root
+        queue = [root]
+        while queue:
+            length = len(queue)
+            for i in range(length):
+                node = queue.pop(0)
+                if i < length - 1:
+                    node.next = queue[0]
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return root
