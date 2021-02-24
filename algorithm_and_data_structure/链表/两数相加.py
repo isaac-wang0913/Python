5008b698e0ece0f8112b64b665f7fb268fff5756
isaac-wang0913 <74162153+isaac-wang0913@users.noreply.github.com>
2021-02-24 10:40:33 +0800

    Update 两数相加.py

--- algorithm_and_data_structure/链表/两数相加.py
+++ algorithm_and_data_structure/链表/两数相加.py
@@ -11,16 +11,18 @@ class Solution:
         # 创建一个伪结点dummy和cur指向头结点, dummy用来最后返回,cur用来遍历
         dummy = ListNode(0)
         cur = dummy
-        carry = 0   # 初始化进位为 0
-        while l1 or l2:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
+        carry = 0   # 初始化进位为0
+        while l1 or l2:     # l1 l2 有一个未结束，继续遍历
+            val1 = l1.val if l1 else 0  # 结束的链表值为0
+            val2 = l2.val if l2 else 0  # 结束的链表值为0
             total = val1 + val2 + carry
-            carry = total // 10
-            cur.next = ListNode(total%10)
+            carry = total // 10     # 进位处理
+            cur.next = ListNode(total % 10)      # 返回结果链表的下一位,防止溢出
             cur = cur.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        if carry > 0:
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        if carry > 0:   # 退出循环时，对最后进位的处理
             cur.next = ListNode(carry)
         return dummy.next
