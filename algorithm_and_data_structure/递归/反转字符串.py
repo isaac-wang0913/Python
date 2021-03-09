02effdf11e9317b2eac4187901c80f2849a7cfc4
isaac-wang0913 <74162153+isaac-wang0913@users.noreply.github.com>
2021-03-09 15:08:02 +0800

    Create 反转字符串.py

---
+++ algorithm_and_data_structure/递归/反转字符串.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # 双指针
+        # 时间复杂度：O(n)
+        # 空间复杂度：O(1)
+        left, right = 0, len(s)-1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        return s
