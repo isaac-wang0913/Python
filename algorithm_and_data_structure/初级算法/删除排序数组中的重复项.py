c2c21cd9502e8ca634fbc63c66ffa37192b862d4
isaac-wang0913 <74162153+isaac-wang0913@users.noreply.github.com>
2021-03-12 10:10:14 +0800

    Update 删除排序数组中的重复项.py

--- algorithm_and_data_structure/初级算法/删除排序数组中的重复项.py
+++ algorithm_and_data_structure/初级算法/删除排序数组中的重复项.py
@@ -1,21 +1,25 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # 双指针
-        # 时间复杂度：O(n)
-        # 空间复杂度：O(1)
-        # while 循环
-        i, j = 0, 1
-        n = len(nums)
-        while j < n:
-            if nums[i] != nums[j]:
-                i += 1
-                nums[i] = nums[j]
-                j += 1
-            else:
-                j += 1
-        return i+1
+        # # 双指针
+        # # 时间复杂度：O(n)
+        # # 空间复杂度：O(1)
+        # # while 循环
+        # if not nums:
+        #     return 0
+        # i, j = 0, 1
+        # n = len(nums)
+        # while j < n:
+        #     if nums[i] != nums[j]:
+        #         i += 1
+        #         nums[i] = nums[j]
+        #         j += 1
+        #     else:
+        #         j += 1
+        # return i+1
 
         # for 循环
+        if not nums:
+            return 0
         i = 0
         n = len(nums)
         for j in range(1, n):
