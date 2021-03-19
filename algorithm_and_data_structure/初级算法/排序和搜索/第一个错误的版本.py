5130799431b01da5b7ea9d39114cdca15228b841
isaac-wang0913 <74162153+isaac-wang0913@users.noreply.github.com>
2021-03-19 11:34:08 +0800

    Create 第一个错误的版本.py

---
+++ algorithm_and_data_structure/初级算法/排序和搜索/第一个错误的版本.py
@@ -0,0 +1,22 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # 二分法
+        # 时间复杂度：O(logn)
+        # 空间复杂度：O(1)
+        l, r = 1, n
+        while l < r:
+            mid = (l + r) // 2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
