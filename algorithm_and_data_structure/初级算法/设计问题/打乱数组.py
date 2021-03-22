fb51428aadf8a9ded2d61b5843138429c1d98607
isaac-wang0913 <74162153+isaac-wang0913@users.noreply.github.com>
2021-03-22 11:40:48 +0800

    Create 打乱数组.py

---
+++ algorithm_and_data_structure/初级算法/设计问题/打乱数组.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.original = nums
+        
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return self.original
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        # 洗牌算法
+        nums = list(self.original)
+        n = len(nums)
+        for i in range(n):
+            j = random.randint(i, n-1)
+            nums[i], nums[j] = nums[j], nums[i]
+        return nums
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
